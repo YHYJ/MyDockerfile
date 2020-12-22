03fa7c97ec625b74fb9bbe1b9213842efd955d1a
YJ-Work <yj1516268@outlook.com>
2020-12-22 19:11:51 +0800

    OK

--- build_base-python_image.sh
+++ build_base-python_image.sh
@@ -28,22 +28,22 @@ readonly name=$(basename "$0")
 # 用户名
 readonly username=$DOCKER_USERNAME
 # 要拉取的官方python镜像的tag
-readonly python_name_alpine="python"
-readonly python_tag_alpine="3.7-alpine3.11"
+readonly base_image_name="python"
+readonly base_image_tag="3.7-alpine3.11"
 # 要构建的base-python镜像的name和tag
-readonly base_python_name_alpine="$username/base-python"
-readonly base_python_tag_alpine="$python_tag_alpine"
+readonly image_name="$username/base-python"
+readonly image_tag="$base_image_tag"
 
 ####################################################################
 #++++++++++++++++++++++++++++++ Main ++++++++++++++++++++++++++++++#
 ####################################################################
 echo -e "Running \\e[01;32m$name\\e[0m ...\n"
 
-# 构建alpine版base-python镜像：使用Dockerfile_base-python_alpine文件进行构建并传递基础镜像名称变量base_image_tag，构建的镜像名为base-python:$python_tag_alpine
-echo -e "\\e[01mBuilding\\e[0m \\e[01;34m$base_python_name_alpine:$base_python_tag_alpine\\e[0m ..."
+# 构建alpine版base-python镜像：使用Dockerfile_base-python_alpine文件进行构建并传递基础镜像名称变量base_image_tag，构建的镜像名为base-python:$base_image_tag
+echo -e "\\e[01mBuilding\\e[0m \\e[01;34m$image_name:$image_tag\\e[0m ..."
 docker buildx build --file ./dockerfile/Dockerfile_base-python_alpine \
                     --platform linux/amd64,linux/arm/v7 \
-                    --tag "$base_python_name_alpine:$base_python_tag_alpine" \
-                    --build-arg base_image_name="$python_name_alpine" \
-                    --build-arg base_image_tag="$python_tag_alpine" \
+                    --build-arg base_image_name="$base_image_name" \
+                    --build-arg base_image_tag="$base_image_tag" \
+                    --tag "$image_name:$image_tag" \
                     . --push

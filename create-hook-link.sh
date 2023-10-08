379cd863f722841ac182f9c2ee5664fe2851fdf2
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-08 14:40:41 +0800

    E: create-hook-link.sh —— 文件重命名

--- create-hook-link.sh
+++ create-hook-link.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 : << !
-Name: create.hook.sh
+Name: create-hook-link.sh
 Author: YJ
 Email: yj1516268@outlook.com
 Created Time: 2023-09-18 13:37:04
 
-Description: 创建post-checkout钩子
+Description: 创建git钩子
 
 Attentions:
 -
